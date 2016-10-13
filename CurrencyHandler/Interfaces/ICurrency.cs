d069a44e4e3e2cf7b2f581d6950cc3be00426ddb
simoneslatnpou <svbyogibear@gmail.com>
2016-10-13 17:44:17 +0200

    Added interface for currency and money class to map to.

---
+++ CurrencyHandler/Interfaces/ICurrency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CurrencyHandler.Interfaces {
+    public interface ICurrency {
+        bool Negativity { get; set; }
+        string Currency { get; set; }
+        string WholeValue { get; set; }
+        string DecimalValue { get; set; }
+    }
+}
