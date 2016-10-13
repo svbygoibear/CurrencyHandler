2d7e951c8851abc390f959bc2c3a2510a9f193ec
simoneslatnpou <svbyogibear@gmail.com>
2016-10-13 18:18:09 +0200

    Started with converter.

---
+++ CurrencyHandler/Helpers/MyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CurrencyHandler.Helpers {
+    public static class MyExtensions {
+        /// <summary>
+        /// This method is used to convert string values into real money objects
+        /// </summary>
+        /// <param name="value">String item to be converted</param>
+        /// <returns>RealMoney object</returns>
+        public static RealMoney stringToRealMoney(this string value) {
+            var res = new RealMoney();
+
+            return res;
+        }
+    }
+}
